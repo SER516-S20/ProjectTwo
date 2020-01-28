2dfbd151bdc6efb1be54916f157df33788814a89
ksharm38 <kshrm38@asu.edu>
2020-01-27 21:28:01 -0700

    Main window code

---
+++ Team_02/MainWindow.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProjectTwo.Team_02;
+import javax.swing.*;
+
+
+/**
+ *
+ * @author Kunal Sharma
+ * created - 01-27-2020
+ */
+public class MainWindow {
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("MouseListener");
+        frame.setVisible(true);
+        frame.setSize(800,1000);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       // ToolKit objToolKit =new ToolKit();
+       // frame.add(ToolKit);
+       // objDraw.drawing();
+    }
+    
+}
