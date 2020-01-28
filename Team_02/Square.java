4c513f58c53e4db535eb5539d330b637835d9f15
Sarvansh Prasher <sarvanshp@gmail.com>
2020-01-28 09:28:38 -0700

    Added Square and rectange shapes

---
+++ Team_02/Square.java
@@ -0,0 +1,70 @@
+/**
+* Created on January 27,2020
+* @author Sarvansh Prasher
+*/
+
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Square extends JFrame {
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	* Launch the application.
+	*/
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Square frame = new Square();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	* Create the frame.
+	*/
+	public Square() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+
+		contentPane = new JPanel() {
+			private static final long serialVersionUID = 1L;
+			public void paintComponent(Graphics g) {
+				Graphics2D g2 = (Graphics2D) g;
+				Shape sqr = new Rectangle(20, 200, 60, 60);
+				g2.draw(sqr);
+			}
+		};
+		contentPane.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("Shape clicked"+ e);
+			}
+		});
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
+
+}
