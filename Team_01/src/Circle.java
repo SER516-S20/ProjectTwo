0d1385439c3ad1e11d0b810b5059f4d703d48c75
Raghavan SS <rsampige@asu.edu>
2020-01-28 20:30:15 -0700

    Added comments for circle, square and triangle classes

--- Team_01/src/Circle.java
+++ Team_01/src/Circle.java
@@ -3,8 +3,18 @@ import java.awt.Graphics2D;
 import java.awt.Shape;
 import java.awt.geom.Ellipse2D;
 
+/**
+ * @author Raghavan
+ *
+ */
 public class Circle implements Shapes {
 
+	/**
+	 * Draws a Circle using Ellipse2D
+	 * @param graphic
+	 * @param x - x coordinate for the shape
+	 * @param y - y coordinate for the shape
+	 */
 	@Override
 	public void drawShape(Graphics graphic, double x, double y) {
 		Shape circle = new Ellipse2D.Double(x, y, 200, 200);
--- Team_01/src/Triangle.java
+++ Team_01/src/Triangle.java
@@ -2,8 +2,18 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Path2D;
 
+/**
+ * @author Raghavan
+ *
+ */
 public class Triangle implements Shapes {
 
+	/**
+	 * Draw a triangle using Path2D
+	 * @param graphic
+	 * @param x - x coordinate for the shape
+	 * @param y - y coordinate for the shape
+	 */
 	@Override
 	public void drawShape(Graphics graphic, double x, double y) {
 		Path2D path = new Path2D.Double();
