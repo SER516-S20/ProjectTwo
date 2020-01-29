3c2eea6a9bcc1d13bb706f2264e83bf050cfe6ce
Somesh Siddabasappa <somesh@someshs-mbp.3520.dhcp.asu.edu>
2020-01-29 14:18:17 -0700

    Frame, SplitPanel and LeftPanel

--- Team_06/src/ObjectInterface.java
+++ Team_06/src/ObjectInterface.java
@@ -1,40 +1,18 @@
-import java.awt.*;
-import java.awt.geom.Ellipse2D;
-import javax.swing.*;
+import java.awt.Dimension;
 
-public class ObjectInterface extends JButton{
-    public ObjectInterface(String label) {
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.border.Border;
+
+public abstract class ObjectInterface extends JButton{
+	public ObjectInterface(String label) {
         super(label);
         Dimension size = getPreferredSize();
         size.width = size.height = Math.max(size.width,size.height);
         setPreferredSize(size);
-
+        
+        Border emptyBorder = BorderFactory.createEmptyBorder();
+        setBorder(emptyBorder);
         setContentAreaFilled(false);
     }
-
-    protected void paintComponent(Graphics g) {
-        if (getModel().isArmed()) {
-            g.setColor(Color.lightGray);
-        } else {
-            g.setColor(getBackground());
-        }
-        g.fillOval(0, 0, getSize().width-1,getSize().height-1);
-
-        super.paintComponent(g);
-    }
-
-    protected void paintBorder(Graphics g) {
-        g.setColor(getForeground());
-        g.drawOval(0, 0, getSize().width-1,     getSize().height-1);
-    }
-
-    Shape shape;
-    public boolean contains(int x, int y) {
-        if (shape == null ||
-                !shape.getBounds().equals(getBounds())) {
-            shape = new Ellipse2D.Float(0, 0, getWidth(), getHeight());
-        }
-        return shape.contains(x, y);
-    }
-
 }
