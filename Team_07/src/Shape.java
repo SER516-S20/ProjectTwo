4ef5fe11960cc6a89be9f071ba9e8f27b1d03850
creativebuilder <csaravindt@gmail.com>
2020-01-29 14:25:55 -0700

    Added the shape interface

---
+++ Team_07/src/Shape.java
@@ -0,0 +1,11 @@
+import java.awt.Graphics;
+
+/*
+ * @author Aravind Thillai Villalan
+ * @since 01-29-2020
+ */
+
+public interface Shape{
+	public void drawShape(Graphics graphic, double x, double y);
+	
+}
