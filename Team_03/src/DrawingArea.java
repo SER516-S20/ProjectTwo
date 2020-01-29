34dfedfc301ad1d3f9f7c443e567b1770722acae
Ashwin <ashwinsrinivasan97@gmail.com>
2020-01-30 01:23:49 +0530

    Added Class to identify the selected shapes and changes made to MouseListener Class

--- Team_03/src/DrawingArea.java
+++ Team_03/src/DrawingArea.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 import java.awt.*;
 
 /**
- * @author srinivasan sundar
+ * @author srinivasan sundar,Ashwin Srinivasan
  * @since 01/26/2020
  * @version 1.0
  */
@@ -12,6 +12,9 @@ public class DrawingArea extends JPanel {
     DrawingArea(){
         this.setBackground(Color.WHITE);
         this.setPreferredSize(new Dimension(800,600));
+        MouseListener ml = new MouseListener();
+        addMouseListener(ml);
+        addMouseMotionListener(ml);
     }
 
 }
--- Team_03/src/MouseListener.java
+++ Team_03/src/MouseListener.java
@@ -4,18 +4,39 @@
  * @version 1.0
  */
 
-import java.awt.*;
-import java.awt.event.MouseAdapter;
+import java.awt.Point;
 import java.awt.event.MouseEvent;
+import javax.swing.event.MouseInputAdapter;
 
-public class MouseListener extends MouseAdapter {
-    private Point startingPoint;
-    public void mousePressed(MouseEvent e)
-    {
-        int x = e.getX();
-        int y = e.getY();
-        Square square = new Square();
-        square.setPosition(x,y);
-        new DrawingArea().addSquare(square);
+/**
+ * This class is used to read the x,y coordinates mouse click and display the selected shape on the DrawingArea
+ */
+
+class MouseListener extends MouseInputAdapter {
+    private Point startPoint;
+
+    public void mousePressed(MouseEvent e) {
+        startPoint = e.getPoint();
+
+        int x = Math.min(startPoint.x, e.getX());
+        int y = Math.min(startPoint.y, e.getY());
+
+        if (JButtonActionListener.isSquarePanelClicked) {
+
+            Square square = new Square();
+            square.setPosition(x, y);
+            Frame.drawingArea.addSquare(square);
+        } else if (JButtonActionListener.isCirclePanelClicked) {
+            Circle circle = new Circle();
+            circle.setPosition(x, y);
+            Frame.drawingArea.addCircle(circle);
+
+        } else {
+            Triangle triangle = new Triangle();
+            triangle.setPosition(x, y);
+            Frame.drawingArea.addTriangle(triangle);
+        }
+
     }
 }
+
