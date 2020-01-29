eb240a53a652565ddbd75d40604662a5b8193548
Yijian Hu <yijianhu@asu.edu>
2020-01-28 20:55:26 -0700

    added leftpanel code for review

---
+++ Team_05/ProjectTwo/src/LeftPanel.java
@@ -0,0 +1,37 @@
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+
+import javax.swing.JPanel;
+
+public class LeftPanel extends JPanel
+{
+	RoundButton roundButton = new RoundButton("Round");
+	TriangleButton triangleButton = new TriangleButton("Triangle");
+	RectangleButton rectangleButton = new RectangleButton("Rectangle");
+	
+	public LeftPanel()
+	{
+		GridLayout gridLayout = new GridLayout(3,1);
+		this.add(roundButton);
+		this.add(triangleButton);
+		this.add(rectangleButton);
+		this.setLayout(gridLayout);
+		
+	}
+	
+	public void SetMouseAdapter_RoundButton(MouseAdapter adapter) 
+	{
+		roundButton.addMouseListener(adapter);
+	}
+	
+	public void SetMouseAdapter_TriangleButton(MouseAdapter adapter) 
+	{
+		triangleButton.addMouseListener(adapter);
+	}
+	
+	public void SetMouseAdapter_RectangleButton(MouseAdapter adapter) 
+	{
+		rectangleButton.addMouseListener(adapter);
+	}
+}
