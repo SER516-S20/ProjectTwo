b1eb7a5852ec94cd3df2be7089ac8ead9b685b7b
san1197 <sandya7c@gmail.com>
2020-01-28 23:03:27 -0700

    Updating comments

--- Team_08/src/Shapes/OvalShape.java
+++ Team_08/src/Shapes/OvalShape.java
@@ -17,8 +17,8 @@ public class OvalShape extends Shape {
         // Check whether (x,y) is inside this oval.
         double rx = width/2.0;   // horizontal radius of ellipse
         double ry = height/2.0;  // vertical radius of ellipse
-        double cx = left + rx;   // x-coord of center of ellipse
-        double cy = top + ry;    // y-coord of center of ellipse
+        double cx = left + rx;   // x-coord of ellipse radius
+        double cy = top + ry;    // y-coord of ellipse radius
         if ( (ry*(x-cx))*(ry*(x-cx)) + (rx*(y-cy))*(rx*(y-cy)) <= rx*rx*ry*ry )
             return true;
         else
