1b1fd0a08e7100d37f22df8698aaa637357e4aa1
Nikitha-ch <38910486+Nikitha-ch@users.noreply.github.com>
2020-01-30 14:17:52 -0700

    Added frame and Options Panel files

---
+++ Team_04/Frame.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Frame extends JFrame
+{
+	private static final String FRAME_TITLE = "Team4";
+	
+
+	public Frame()
+	{
+	    this.setTitle(FRAME_TITLE);
+	    this.setMinimumSize(new Dimension(800, 600));
+	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    Container content = this.getContentPane();
+	    content.setLayout(new BoxLayout(content, BoxLayout.X_AXIS));
+	    content.add(new OptionsPanel());
+	    content.add(new DrawingCanvas());
+	    this.pack();
+	    this.setLocationRelativeTo(null);
+	    this.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		    new Frame();
+	}
+}
