46603ac6d0b0df02fb4bdd6c7ef5e37b338dafca
Chanyadav <yadavchan23@gmail.com>
2020-01-30 11:01:16 -0700

    Changes made for MouseListener Class

--- Team_03/src/MouseListener.java
+++ Team_03/src/MouseListener.java
@@ -1,45 +1,68 @@
-/**
- * @author Ashwin Srinivasan
- * @since 01/28/2020
- * @version 1.0
- */
-
 import java.awt.Point;
 import java.awt.event.MouseEvent;
-import javax.swing.*;
+
+import javax.swing.JOptionPane;
 import javax.swing.event.MouseInputAdapter;
 
-/**
- * This class is used to read the x,y coordinates mouse click and display the selected shape on the DrawingArea
- */
-
+/*This class is used to drag a particular shape and  create a shape on mouse click*/
 class MouseListener extends MouseInputAdapter {
-    private Point startPoint;
-    static final String INFO_MESSAGE = "Please select a shape";
+	private Point startPoint;
+	static boolean activateDrag = false;
+	static final String INFO_MESSAGE = "Please select a shape";
 
-    public void mousePressed(MouseEvent e) {
-        startPoint = e.getPoint();
+	public void mousePressed(MouseEvent e) {
+		startPoint = e.getPoint();
 
-        int x = Math.min(startPoint.x, e.getX());
-        int y = Math.min(startPoint.y, e.getY());
+		int cordinateX = Math.min(startPoint.x, e.getX());
+		int cordinateY = Math.min(startPoint.y, e.getY());
 
-        if (JButtonActionListener.isSquarePanelClicked) {
+		if (new Boundary().checkBoundary(cordinateX, cordinateY)) {
+			activateDrag = true;
+		} else {
+			activateDrag = false;
+			if (JButtonActionListener.isSquarePanelClicked) {
 
-            Square square = new Square();
-            square.setPosition(x, y);
-            Frame.drawingArea.addSquare(square);
-        } else if (JButtonActionListener.isCirclePanelClicked) {
-            Circle circle = new Circle();
-            circle.setPosition(x, y);
-            Frame.drawingArea.addCircle(circle);
+				Square square = new Square();
+				square.setPosition(cordinateX, cordinateY);
+				Frame.drawingArea.addSquare(square);
 
-        } else if (JButtonActionListener.isTrianglePanelClicked) {
-            Triangle triangle = new Triangle();
-            triangle.setPosition(x, y);
-            Frame.drawingArea.addTriangle(triangle);
-        } else {
-            JOptionPane.showMessageDialog(null, INFO_MESSAGE);
-        }
-    }
+			} else if (JButtonActionListener.isCirclePanelClicked) {
+
+				Circle circle = new Circle();
+				circle.setPosition(cordinateX, cordinateY);
+				Frame.drawingArea.addCircle(circle);
+
+			} else if (JButtonActionListener.isTrianglePanelClicked) {
+
+				Triangle triangle = new Triangle();
+				triangle.setPosition(cordinateX, cordinateY);
+				Frame.drawingArea.addTriangle(triangle);
+			} else {
+				JOptionPane.showMessageDialog(null, INFO_MESSAGE);
+
+			}
+		}
+
+	}
+
+	public void mouseDragged(MouseEvent event) {
+
+		if (activateDrag) {
+			int cordinateX = event.getX();
+			int cordinateY = event.getY();
+
+			Object object = Boundary.selectedObject;
+			if (object instanceof Square) {
+				Square square = (Square) object;
+				square.setPosition(cordinateX, cordinateY);
+				Frame.drawingArea.repaint();
+			} else if (object instanceof Circle) {
+				Circle circle = (Circle) object;
+				circle.setPosition(cordinateX, cordinateY);
+				Frame.drawingArea.repaint();
+			}
+		}
+
+	}
+
 }
-
