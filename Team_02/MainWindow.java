327694919d3693ba3df47d45824f27aad859edd5
Srinivasan Sundar <srini@srinivasans-mbp.3500.dhcp.asu.edu>
2020-01-30 12:25:26 -0700

    Fetching master branch to Team_03 branch

--- Team_02/MainWindow.java
+++ Team_02/MainWindow.java
@@ -1,27 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ProjectTwo.Team_02;
 import javax.swing.*;
 
-
 /**
- *
- * @author Kunal Sharma
- * created - 01-27-2020
+ * @author Kunal Sharma created - 01-27-2020
  */
+
 public class MainWindow {
-    public static void main(String[] args)
-    {
-        JFrame frame = new JFrame("MouseListener");
-        frame.setVisible(true);
-        frame.setSize(800,1000);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       // ToolKit objToolKit =new ToolKit();
-       // frame.add(ToolKit);
-       // objDraw.drawing();
-    }
-    
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Main Window");
+		frame.setVisible(true);
+		frame.setSize(1000, 1000);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JSplitPane splitPane = new JSplitPane();
+		splitPane.setSize(900, 900);
+		splitPane.setDividerSize(0);
+		splitPane.setDividerLocation(200);
+		splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+
+		splitPane.setTopComponent(new PanelToolkit());
+		splitPane.setBottomComponent(new DrawBoardPanel());
+		frame.add(splitPane);
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+	}
+
 }
