2059b985e159d8cf8bb3a2a69f2fc2a4465c1179
Yijian Hu <yijianhu@asu.edu>
2020-01-29 19:09:36 -0700

    Update RightPanel.java

--- Team_05/ProjectTwo/src/RightPanel.java
+++ Team_05/ProjectTwo/src/RightPanel.java
@@ -1,32 +1,44 @@
 import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
+import java.util.Hashtable;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class RightPanel extends JPanel{
-	private RoundButton roundButton = new RoundButton("Round");
-	private TriangleButton triangleButton = new TriangleButton("Triangle");
-	private RectangleButton rectangleButton = new RectangleButton("Rectangle");
+	private Hashtable<Integer, JButton> shapes;
+	
 	public RightPanel() {
+		shapes = new Hashtable<Integer, JButton>();
 		
 	}
 	public void AddRound() {
-		
+		RoundButton round = new RoundButton("");
+		round.addMouseListener(new MouseAdapter() {});
+		shapes.put(round.hashCode(),round);
+		this.add(round);
 	}
 	
 	public void AddTriangle()
 	{
-		
+		TriangleButton triangle = new TriangleButton("");
+		triangle.addMouseListener(new MouseAdapter() {});
+		shapes.put(triangle.hashCode(),triangle);
+		this.add(triangle);
 	}
 	
 	public void AddRectangle()
 	{
-		
+		RectangleButton rectangle = new RectangleButton("");
+		rectangle.addMouseListener(new MouseAdapter() {});
+		shapes.put(rectangle.hashCode(),rectangle);
+		this.add(rectangle);
 	}
 	
 	public void DeleteShape(int hashCode)
 	{
-		
+		this.remove(shapes.get(hashCode));
+		shapes.remove(hashCode);
 	}
 }
