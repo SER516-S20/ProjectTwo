6ba7b2726c29f86dcdb13d58dd974d68a6f9691b
adityab079 <36786132+adityab079@users.noreply.github.com>
2020-01-30 13:51:27 -0700

    Update RightPanel.java

--- Team_07/src/RightPanel.java
+++ Team_07/src/RightPanel.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
 public class RightPanel implements Panel {
     JPanel panel;
 
