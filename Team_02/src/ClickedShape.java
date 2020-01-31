d324e0eafd739e74dd47968ae1cd43062c4e5b7d
Sarvansh prasher <sarvanshp@gmail.com>
2020-01-30 17:47:27 -0700

    Update ClickedShape.java

--- Team_02/src/ClickedShape.java
+++ Team_02/src/ClickedShape.java
@@ -1,10 +1,11 @@
 /**
  * @author Suryadeep 
  * @created on 01-27-2020
+ * version 1.0
  */
 public class ClickedShape {
 	public static String shapeName;
-
+	
 	public String returnShape(String clickedShape) {
 		try {
 			shapeName = clickedShape;
