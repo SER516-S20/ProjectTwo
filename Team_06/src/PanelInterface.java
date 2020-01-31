67a8c6de0448de2c73dbbfa4a651f1f1e215b673
Sheran-12 <54861754+Sheran-12@users.noreply.github.com>
2020-01-30 18:08:21 -0700

    Update PanelInterface.java

--- Team_06/src/PanelInterface.java
+++ Team_06/src/PanelInterface.java
@@ -1,10 +1,18 @@
 import java.awt.*;
 import javax.swing.*;
 
+/**
+ * author: Somesh, Suyog
+ * Date: 01/28/2020
+ * Description: This is the abstract class for the two panels used in the FrameCLass. It extends JPanel and sets the background color for the panels.
+ */
 public abstract class PanelInterface extends JPanel{
-	
+
+	//This is the constructor for the PanelInterface abstract class.
 	PanelInterface(String s, int a, int b){
+
 		this.setBackground(Color.decode(s));
+
 	}
-	
+
 }
