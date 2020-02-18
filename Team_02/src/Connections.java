543f4965dbb8fcbef6ca0fdd5f70c219c9356e9d
ksharm38 <54971135+ksharm38@users.noreply.github.com>
2020-02-17 19:34:30 -0700

    Changed type of graphics parameter

--- Team_02/src/Connections.java
+++ Team_02/src/Connections.java
@@ -19,7 +19,25 @@ class Connections extends JFrame{
         
     }
 
-    public void DotToDOtConnectin(Graphics g, int x1, int y1, int x2, int y2)
+    public void DotToDOtConnectin(Graphics2D g, int x1, int y1, int x2, int y2)
+    {
+        try{
+            drawLines(g, x1, y1, x2, y2);
+        }
+        catch(Exception ex){
+            
+        }
+    }
+    public void DotToBarConnection(Graphics2D g, int x1, int y1, int x2, int y2)
+    {
+        try{
+            
+        }
+        catch(Exception ex){
+            
+        }
+    }
+    public void BarToBarConnection(Graphics2D g, int x1, int y1, int x2, int y2)
     {
         try{
         }
@@ -27,29 +45,12 @@ class Connections extends JFrame{
             
         }
     }
-    public void DotToBarConnection(Graphics g, int x1, int y1, int x2, int y2)
-    {
-        try{
-            
-        }
-        catch(Exception ex){
-            
-        }
-    }
-    public void BarToBarConnection(Graphics g, int x1, int y1, int x2, int y2)
-    {
-        try{
-        }
-        catch(Exception ex){
-            
-        }
-    }
-    void drawLines(Graphics g, int x1, int y1, int x2, int y2) {
+    void drawLines(Graphics2D g, int x1, int y1, int x2, int y2) {
       try{
-          Graphics2D g2d = (Graphics2D) g;
+         // Graphics2D g2d = (Graphics2D) g;
       
  
-        g2d.drawLine(x1, y1, x2, y2);
+        g.drawLine(x1, y1, x2, y2);
       }
       catch(Exception ex)
       {
